7d0249aa2e2f0263df06cd9f97c733240ddbdcd5
Ubuntu <ubuntu@ip-172-31-0-220.us-west-1.compute.internal>
2021-10-21 07:34:12 +0000

    jwt review

--- bookstore/app.py
+++ bookstore/app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
+import jwt
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+
+secret = "hjbsdfhbvfdsaoibvdfsiobvfds"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    print(request.method)
+    if request.method == 'POST':
+        jwt_str = jwt.encode({"username": "benjamin"}, secret, algorithm="HS256")
+        return jsonify({"jwt": jwt_str})
+    else:
+        return render_template("login.html", page_name="Login")
+            
+
 
 @app.route("/")
 @app.route("/store", methods=["GET"])
@@ -11,11 +26,14 @@ def store():
 def home():
     return "home"
     
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    return "login"
-    
 @app.route("/signup", methods=["GET", "POST"])
 @app.route("/register", methods=["GET", "POST"])
 def signup():
     return "signup"
+    
+@app.errorhandler(404)
+def not_found():
+    return "404 not found"
+    
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
