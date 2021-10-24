5a2b54df4facf797de47075a70741f85bc7f8b09
Ubuntu <ubuntu@ip-172-31-0-220.us-west-1.compute.internal>
2021-10-24 09:09:04 +0000

    adds a secretary who takes care of encoding, decoding and salting things

---
+++ bookstore/security/Secretary.py
@@ -0,0 +1,38 @@
+import jwt
+import bcrypt
+from .SecretKeeper import SecretKeeper
+
+class Secretary:
+
+    __jwt_key = None
+    __jwt_algorithm = None
+    
+    def __init__(self):
+        '''Uses jwt and bcrypt to ecode and decode data as well as checking peoiple's passwords'''
+
+        jwt_secrets = SecretKeeper.get("jwt_secrets")
+        self.__jwt_key = jwt_secrets.get("key")
+        self.__jwt_algorithm = jwt_secrets.get("algorithm")
+
+    def encode(self, data: dict) -> str:
+        '''Uses jwt to encode the data into a token'''
+
+        return jwt.encode(data, self.__jwt_key, algorithm=self.__jwt_algorithm)
+
+    def decode(self, token: str) -> dict:
+        '''Uses jwt to decode token'''
+
+        return jwt.decode(token, self.__jwt_key, algorithms=[self.__jwt_algorithm])
+
+    def hash_password(self, password: str) -> str:
+        '''Uses bcrypt to hash password'''
+
+        salt = bcrypt.gensalt(rounds=10)
+        return bcrypt.hashpw(bytes(password, 'utf-8'), salt)
+
+    def check_password(self, password: str, hashed_password) -> bool:
+        '''Uses bcrypt to check password'''
+
+        return bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password)
+    
+    
