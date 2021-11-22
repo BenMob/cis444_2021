15bf59e13a0d6ebba6d923d11d3bea69270569a4
Ubuntu <ubuntu@ip-172-31-0-220.us-west-1.compute.internal>
2021-11-22 01:33:42 +0000

    refactors app.py to use the new framework

--- bookstore-new-design/app.py
+++ bookstore-new-design/app.py
@@ -1,43 +1,83 @@
-from flask import Flask, Blueprint ,render_template, jsonify, request
-from security import Database, Queries
-from controllers import UserController, BookController, TransactionController
+from flask import Flask, render_template, g
+from persistance import Database, Queries
+from tools.logging import logger
+from tools.security import token_required
+import traceback
 
 # Sets up app and db
-app = Flask(__name__, template_folder="templates")
-db_conn = Database.get_connection()
-queries = Queries(db_conn)
-user_controller = UserController(queries)
-book_controller = BookController(queries)
+app = Flask(__name__, template_folder="static")
+db_conn = Database.get_connection()            # Connects to the database 
+open_api_route = "/open_api/<route_name>"      # Route format for open api
+secure_api_route = "/secure_api/<route_name>"  # Route format for secure api 
+error_message = "Oups! something went wrong."  # Error message
 
-@app.route("/")
-def index(methods=["GET"]):
+
+# Environment initializer
+def init_new_env():
+    '''Intializes a new global environment'''
+    
+    if 'queries' not in g:
+        g.queries = Queries(db_conn)
+
+@app.route("/", methods=["GET"])
+def index():
+    '''Renders index.html file from static'''
+    
     return render_template("index.html")
 
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    return user_controller.login(username, password)
+@app.route(open_api_route, methods=["POST"])
+def open_api(route_name):
+    '''Directs Open API Calls'''
+    logger.debug("Received an open Request.")
 
-@app.route("/signup", methods=["POST"])
-def signup():
-    first_name = request.json.get("first_name")
-    last_name = request.json.get("last_name")
-    username = request.json.get("username")
-    password = request.json.get("password")
-    
-    return user_controller.signup(
-        first_name,
-        last_name,
-        username,
-        password
-    )
-    
-@app.route("/books", methods=["POST"])
-def books():
-    if(user_controller.isLoggedIn(request.json.get("access_token"))):
-        return book_controller.get_all()
-    return user_controller.isNotLoggedIn();
+    # Initializes new environment
+    init_new_env()
+    response = None
+
+    try:
+        # Process the request
+        package = __import__(f"open_routes.{route_name}")
+        handler = getattr(package, route_name)
+        response = handler.handle_request()
+        
+    except Exception as error:
+        # Something went wrong 
+        log = str(Exception) + "\n"
+        log += str(error) + "\n"
+        log += traceback.format_exc()
+        logger.error(log)
+        reponse = jsonify({"status": 500, "data": error_message})
+
+    # Return the response
+    return response
+
+
+@app.route(secure_api_route, methods=["POST"])
+@token_required
+def secure_api(route_name):
+    '''Directs Secure API Calls'''
+    logger.debug("Received a secure request.")
+
+    # Initializes new environment                                                                                                                                     
+    init_new_env()
+    response = None
+
+    try:
+        # Process the request                                                                                                                                          
+        package = __import__(f"secure_routes.{route_name}")
+        handler = getattr(package, route_name)
+        response = handler.handle_request()
+
+    except Exception as error:
+        # Something went wrong                                                                                                                                         
+        log = str(Exception) + "\n"
+        log += str(error) + "\n"
+        log += traceback.format_exc()
+        logger.error(log)
+        reponse = jsonify({"status": 500, "data": error_message})
+
+    # Return the response                                                                                                                                             
+    return response
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
