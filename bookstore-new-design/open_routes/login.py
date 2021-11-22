6f0ab2688a296a0705963544e683da72773b7d2d
Ubuntu <ubuntu@ip-172-31-0-220.us-west-1.compute.internal>
2021-11-22 01:32:19 +0000

    rewrite logs in open routes

--- bookstore-new-design/open_routes/login.py
+++ bookstore-new-design/open_routes/login.py
@@ -1,12 +1,13 @@
-from flask import g, request
+from flask import g, request, jsonify
 from controllers.UserController import UserController
 from tools.logging import logger 
 
 def handle_request():
-    logger.debug("Handling login request")
+    logger.debug("Request was directed to /login endpoint. Beginning handling the request.")
     controller = UserController(g.queries)
     username = request.json.get('username')
     password = request.json.get('password')
-    return controller.login(username, password)
-    
+    response = controller.login(username, password)
+    logger.debug("Finished handling /login request.")
+    return jsonify(response)
     
--- bookstore-new-design/open_routes/signup.py
+++ bookstore-new-design/open_routes/signup.py
@@ -1,17 +1,20 @@
-from flask import g, request
+from flask import g, request, jsonify
 from controllers.UserController import UserController
 from tools.logging import logger
 
 def handle_request():
-    logger.debug("Handling signup request")
+    logger.debug("Request was directed to /signup endpoint. Beginning handling the request.")
     controller = UserController(g.queries)
     first_name = request.json.get("first_name")
     last_name = request.json.get("last_name")
     username = request.json.get("username")
     password = request.json.get("password")
-    return controller.signup(
+    response = controller.signup(
         first_name,
         last_name,
         username,
         password
     )
+    logger.debug("Finished handling /signup request.")
+
+    return jsonify(response)
