95442c8dc99ec42022db4325b4bb3ec2e41f6ba4
Benjamin <omben001@cougars.csusm.edu>
2021-12-08 18:38:08 -0800

    adds tools

---
+++ quicknotes/frontend/src/tools/authprovider.js
@@ -0,0 +1,7 @@
+const login({username, password, onSuccess, onFailure}) = () => {
+    
+}
+
+const signup({firstname, lastname, username, email, onSuccess, onFailure}) = () => {
+
+}
---
+++ quicknotes/frontend/src/tools/colors.js
@@ -0,0 +1,13 @@
+/* Color Styles */
+const colors = {
+  primaryBg: "#314343",
+  secondaryBg: "#FBFDB5",
+  primaryText: "white",
+  secondaryText: "black",
+  primaryBorder: "lightGray",
+  secondaryBorder: "#333",
+  white: "#f2f3f4"
+
+}
+
+export default colors;
