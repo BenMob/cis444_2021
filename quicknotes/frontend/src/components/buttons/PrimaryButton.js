b90a0c3e1c8e9e8b7ef0201ead15234250817d5f
Benjamin <omben001@cougars.csusm.edu>
2021-12-12 12:08:31 -0800

    modifies buttons a little bit

--- quicknotes/frontend/src/components/buttons/PrimaryButton.js
+++ quicknotes/frontend/src/components/buttons/PrimaryButton.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-function PrimaryButton({label, onClick, className}) {
+function PrimaryButton({label, onClick, className, styles}) {
+    const containerStyles = {
+		...styles
+	}
+
     return (
-         <button onClick={onClick} className={`w3-button w3-light-gray w3-round-large w3-border w3-small w3-hover-green w3-margin-left ${className}`}>{label}</button>
+         <button styles={containerStyles} onClick={onClick} className={`w3-button w3-light-gray w3-round-large w3-border w3-small w3-hover-green w3-margin-left ${className}`}>{label}</button>
     )
 }
 
--- quicknotes/frontend/src/components/buttons/SecondaryButton.js
+++ quicknotes/frontend/src/components/buttons/SecondaryButton.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-function SecondaryButton({label, onClick, className}) {
+function SecondaryButton({label, onClick, className, styles}) {
+    const containerStyles = {
+		...styles
+	}
+
     return (
-        <button onClick={onClick} className={`w3-button w3-round-large w3-border w3-margin-left w3-margin-right w3-small w3-hover-light-gray ${className}`}>{label}</button>
+        <button styles={containerStyles} onClick={onClick} className={`w3-button w3-round-large w3-border w3-margin-left w3-margin-right w3-small w3-hover-light-gray ${className}`}>{label}</button>
     )
 }
 
