d4a5b942579a92cfd0175fd4ac860d9407fe7796
Benjamin <omben001@cougars.csusm.edu>
2021-12-14 23:48:03 -0800

    modifies login and signup button

--- quicknotes/frontend/src/tools/authprovider.js
+++ quicknotes/frontend/src/tools/authprovider.js
@@ -1,7 +1,24 @@
-const login({username, password, onSuccess, onFailure}) = () => {
-    
+import UserModel from "../models/UserModel"
+
+const login = (username, password, onSuccess, onFailure) => {
+    onSuccess(UserModel.getSampleUser())
 }
 
-const signup({firstname, lastname, username, email, onSuccess, onFailure}) = () => {
+const signup = (firstname, lastname, username, email, password, messageHandler) => {
+    messageHandler("I can't believe you are trying to create an account and you haven't even attached a backend to this app.", "Bruh!!!")
+}
 
+const isLoggedIn = (user) => {
+    return (
+        user.token !== undefined &&
+        user.id !== undefined &&
+        user.firstName !== undefined &&
+        user.lastName !== undefined
+    )    
+}
+
+export {
+    login,
+    signup,
+    isLoggedIn
 }
