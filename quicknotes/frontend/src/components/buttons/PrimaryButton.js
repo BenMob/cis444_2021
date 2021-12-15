74cea3406a3e7992f0496004e69a28f2966bee2e
Benjamin <omben001@cougars.csusm.edu>
2021-12-14 23:24:03 -0800

    changes style on primary button

--- quicknotes/frontend/src/components/buttons/PrimaryButton.js
+++ quicknotes/frontend/src/components/buttons/PrimaryButton.js
@@ -6,7 +6,7 @@ function PrimaryButton({label, onClick, className, styles}) {
 	}
 
     return (
-         <button styles={containerStyles} onClick={onClick} className={`w3-button w3-light-gray w3-round-large w3-border w3-small w3-hover-green w3-margin-left ${className}`}>{label}</button>
+         <button styles={containerStyles} onClick={onClick} className={`w3-button w3-green w3-round-large w3-border w3-small w3-hover-light-gray w3-margin-left ${className}`}>{label}</button>
     )
 }
 
